75dc51b7667a3955c7212da412858343f0aa66a5
Luisina Bender <57606326+LuuxDD@users.noreply.github.com>
2024-06-17 16:56:36 -0300

    Books Repository

---
+++ EjerciciosDePrueba/Repositories/LibrosRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosDePrueba.Repositories
+{
+    public class LibrosRepository
+    {
+        string urlApi = "https://ingenieriasoftware-9437.restdb.io/home/db/ingenieriasoftware-9437/cards ";
+        HttpClient client = new HttpClient();
+
+        public LibrosRepository()
+        {
+            //Configuramos que trabajara con respuestas JSON
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            client.DefaultRequestHeaders.Add("ApiKay ", "64aeea7da1ce30200009bc63");
+        }
+    }
+}
